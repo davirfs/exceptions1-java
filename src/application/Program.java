6f3a41f03d0d0222ab8edbd3a3b332f91d522e6a
Davi Ribeiro <davirfs@hotmail.com>
2024-12-16 20:59:53 -0300

    Good solution

--- src/application/Program.java
+++ src/application/Program.java
@@ -6,24 +6,23 @@ import java.util.Date;
 import java.util.Scanner;
 
 import model.entities.Reservation;
+import model.exceptions.DomainException;
 
 public class Program {
-	public static void main(String[] args) throws ParseException {
+	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
 		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 		
-		System.out.println("Room Number: ");
-		int number = scanner.nextInt();
+		try {
+			System.out.println("Room Number: ");
+			int number = scanner.nextInt();
+			
+			System.out.println("Check-in date: (dd/MM/yyyy): ");
+			Date checkIn = sdf.parse(scanner.next());
+			
+			System.out.println("Check-out (dd/MM/yyyy): ");
+			Date checkOut = sdf.parse(scanner.next());
 		
-		System.out.println("Check-in date: (dd/MM/yyyy): ");
-		Date checkIn = sdf.parse(scanner.next());
-		
-		System.out.println("Check-out (dd/MM/yyyy): ");
-		Date checkOut = sdf.parse(scanner.next());
-		
-		if(!checkOut.after(checkIn)){
-			System.out.println("Error in reservation: Check-out date must be after check-in date");
-		}else{
 			Reservation reservation = new Reservation(number,checkIn,checkOut);
 			System.out.println(reservation);
 			
@@ -35,15 +34,17 @@ public class Program {
 			System.out.println("Check-out (dd/MM/yyyy): ");
 			checkOut = sdf.parse(scanner.next());
 			
-			String error = reservation.updateDates(checkIn, checkOut);
-			if(error != null){
-				System.out.println("Error in reservation: " + error);
-			}else {
-				System.out.println(reservation);
-			}
-			
-		
-		scanner.close();
+			reservation.updateDates(checkIn, checkOut);
+			System.out.println(reservation);
+		}catch(ParseException e){
+			System.out.println("Invalid date format.");
+		}catch(DomainException e){
+			System.out.println("Error in Reservation: " + e.getMessage());
+		}catch(RuntimeException e){
+			System.out.println("Unexpected error");
 		}
+	
+	scanner.close();
 	}
 }
+
